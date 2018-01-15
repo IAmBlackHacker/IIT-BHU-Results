6fa99f6c90797d9e974d47821a509752080d99f7
Lokesh Jagdish Bhoyar <lokesh.prenoramance@gmail.com>
2018-01-15 15:03:58 +0530

    Add files via upload

--- getr.py
+++ getr.py
@@ -4,18 +4,19 @@ try:
 	import webbrowser
 	import pickle
 except:
-	print('install first hashlib,webbrowser\n   python -m pip install pickle\n   python -m pip install hashlib\n   python -m pip install webbrowser\nThis code will run on python 3.x')
+	print('install first hashlib,webbrowser\n   python3 -m pip install pickle\n   python3 -m pip install hashlib\n   python3 -m pip install webbrowser\nThis code will run on python 3.x')
 	sys.exit(0)
 import os
-N=str(input('Enter Roll : '))
-maps=pickle.load(open('Finals','rb'))
-# for i in open('final.txt').readlines():
-# 	i=i.strip().split()
-# 	maps[i[0]]=i[1]
-if N in maps.keys():
+
+try:
+	N=str(input('Enter Roll : '))
+	maps=pickle.load(open('Finals','rb'))
 	has=hashlib.md5(N.encode('utf-8')).hexdigest()
-	cmd='http://10.1.131.10/grade_sheet/index.php?sname='+N+'&sid='+maps[N]+'&msname='+has+'&ms1=95aea4c3483c560373356d1ba3fd73cc'
-	webbrowser.open(cmd)
-else:
-	print('Enter Valid Roll')
-	os.system('pause')	
+	if has in maps.keys():
+		cmd='http://10.1.131.10/grade_sheet/index.php?sname='+N+'&sid='+maps[has]+'&msname='+has+'&ms1=95aea4c3483c560373356d1ba3fd73cc'
+		webbrowser.open(cmd)
+	else:
+		print('Enter Valid Roll')
+		os.system('pause')	
+except:
+	print('\n\n\t\tTry in python3.x else whats 8419027520')
