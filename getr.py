c4d11f0c3326aa04967730a5c5cf2a25eff3713b
Royal One <24620094+IAmBlackHacker@users.noreply.github.com>
2018-09-25 22:27:13 +0530

    Add files via upload

--- getr.py
+++ getr.py
@@ -12,15 +12,23 @@ if sys.version_info[0] != 3:
 	print('\n------------------------------\n     USE PYTHON 3.x \n------------------------------\n')
 	sys.exit(0)
 
-try:
-	N=str(input('Enter Roll : '))
+def open_roll(N):
 	maps=pickle.load(open('Finals','rb'))
 	has=hashlib.md5(N.encode('utf-8')).hexdigest()
 	if has in maps.keys():
 		cmd='http://10.1.131.10/grade_sheet/index.php?sname='+N+'&sid='+maps[has]+'&msname='+has+'&ms1=95aea4c3483c560373356d1ba3fd73cc'
 		webbrowser.open(cmd)
 	else:
-		print('Enter Valid Roll')
-		os.system('pause')	
+		print('\n\tEnter Valid Roll\t')
+try:
+	N=str(input('Enter Roll : '))
+	open_roll(N)
+except pickle.UnpicklingError:
+	content = open('Finals','rb').read()
+	with open('Finals','wb') as output:
+		for line in content.splitlines():
+			output.write(line + str.encode('\n'))
+	open_roll(N)
 except Exception as ex:
 	print(ex)
+	print('Messege Code Admin ...!')
